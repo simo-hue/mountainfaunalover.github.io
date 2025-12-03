640101c1e81df9ebc3c707d6014d53c92a23763a
simo-hue <mattioli.simone.10@gmail.com>
2025-12-03 16:51:42 +0100

    Maximize llms.txt with FAQ and Blog content

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,11 +2,17 @@ export const dynamic = 'force-static';
 
 export default function robots() {
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-            disallow: "/private/",
-        },
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: "/private/",
+            },
+            {
+                userAgent: ["GPTBot", "Google-Extended", "CCBot"],
+                allow: "/",
+            }
+        ],
         sitemap: "https://mountainfaunalover.github.io/sitemap.xml",
     };
 }
